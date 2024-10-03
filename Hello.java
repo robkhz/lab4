7cd4b92ae4c98da2ed28ca452e1c46fa34a9baa3
Robert Khzouz <rckhzouz@gmail.com>
2024-10-03 16:15:03 -0400

    Removed line

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++) {
             System.out.println("Hello World..."+i);
-            System.out.println("New line for commit!");
         }
     }
 }
